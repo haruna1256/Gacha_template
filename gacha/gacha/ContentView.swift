b63498c8f4606eb9f3663be5efdd15bc7593cf83
haruna1256 <haru_kawagishi@icloud.com>
2025-07-21 16:47:21 +0900

    [add] swiftuiのファイル

---
+++ gacha/gacha/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gacha
+//
+//  Created by 川岸遥奈 on 2025/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gacha/gacha/gachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  gachaApp.swift
+//  gacha
+//
+//  Created by 川岸遥奈 on 2025/07/21.
+//
+
+import SwiftUI
+
+@main
+struct gachaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
